22e1369d4ef7ed471682405c133cf40128d08c09
Benjamin Idewor <benjaminidewor@gmail.com>
2024-04-07 11:47:56 +0200

    fix import error

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from application.service import WeatherForecastService
 import functions_framework
 
 @functions_framework.http
-def main():
+def weather_forecast():
     service = WeatherForecastService()
     service.run()
     return "OK"
